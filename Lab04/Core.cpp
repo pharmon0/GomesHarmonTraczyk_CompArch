1a9b5bb92596d8d79601467f70ea1f2dc05b1f75
Phillip Harmon <pharmon@uncc.edu>
2022-10-23 18:11:12 -0400

    branched to edit Core.cpp

--- Lab04/Core.cpp
+++ Lab04/Core.cpp
@@ -18,11 +18,13 @@ bool Core::process(void){
  //Writeback Stage (Memory Access and Register File Writeback)
    //check portD
     //TODO memory D-Port
+    
    //writeback
     this->rf.rfctrl.rfwen = this->ewr.output.rfctrl.rfwen;
     this->rf.rfctrl.selrd = this->ewr.output.rfctrl.selrd;
     //this->rf.rd = //TODO;
     this-rf.processWrite();
+
    //make request to portD
     //TODO memory D-Port
     this->ctrl.stallW = 0; //TODO stall based on D-Port
@@ -31,6 +33,7 @@ bool Core::process(void){
    //access register file
     this->rf.rfctrl = this->der.output.rfctrl;
     this->rf.processRead();
+
    //run alu
     if(this->der.output.bctrl.halt) return 1; //HALT ON EXECUTE OF HALT INSTRUCTION
     this->alu.A = (this->der.output.aluctrl.aluasel)?(this->der.output.pc):(this->rf.rs1);
@@ -39,6 +42,7 @@ bool Core::process(void){
     this->alu.aluctrl.alucomp = this->der.output.aluctrl.alucomp;
     this->alu.process();
     this->ctrl.stallE = alu.aluctrl.busy;
+
    //save EWRegister
     this->ewr.input.aluX = this->alu.X;
     this->ewr.input.pc   = this->der.output.pc;
@@ -51,6 +55,7 @@ bool Core::process(void){
     //TODO update Decoder to use port->process paradigm instead of process(data)
     this->dec.instruction = this->fdr.output.i;
     this->dec.process();
+
    //save DERegister
     //TEST data hazards
     //TODO update for floating points
@@ -67,6 +72,7 @@ bool Core::process(void){
     this->der.input.rfctrl  = this->dec.rfctrl;
     this->der.input.aluctrl = this->dec.aluctrl;
     this->der.input.memctrl = this->dec.memctrl;
+
     //TODO update Decoder to rename ctrl to bctrl
     this->der.input.bctrl = this->dec.bctrl;
     this->der.input.pc    = this->fdr.output.pc;
@@ -83,6 +89,7 @@ bool Core::process(void){
         this->portI.address      = this->pc;
         this->portI.ctrl.membusy = 1;
     }
+
    //save FDRegister
     this->fdr.ctrl.no_op  = this->ctrl.stallF;
     this->fdr.ctrl.stall  = this->ctrl.stallD
@@ -91,6 +98,7 @@ bool Core::process(void){
     this->fdr.input.i = this->portI.data;
     this->fdr.input.pc = this->pc;
     this->fdr.process();
+
     //Update PC value based on branch
     uint32_t pcplus = (this->der.output.bctrl.btype && this->alu.X)?(immediate):(4U);
     pcplus += this->pc;
